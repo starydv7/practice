d004be36d54a94043cb1ef55a325deb32dabc8ec
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-10-28 01:40:43 +0530

    mappp

---
+++ practiceJS/map.js
@@ -0,0 +1,11 @@
+// forEach and map difference
+
+// var arr=[1,2,4,6,7,8];
+
+function examples(){
+    if(true){
+        var count=10;
+        console.log(count);
+    }
+    console.log(count);
+}
