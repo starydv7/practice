f092825e4ea0af510495fffe5471d6830ec14a74
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-11-03 12:30:24 +0530

    Misising Integers from Odd Array
    Missing odd Number from Odd array

--- practiceJS/thisKey.js
+++ practiceJS/thisKey.js
@@ -27,3 +27,27 @@ const obj1 = { a: 1, b: 2 };
 const obj2 = { b: 3, c: 4 };
 const merged = { ...obj1, ...obj2 };
 console.log("merge",merged);
+
+
+
+// Missing ODD number from array
+function findMissingOddNumbers(arr) {
+  const minVal = Math.min(...arr);
+  const maxVal = Math.max(...arr);
+  
+  const missingOddNumbers = [];
+  
+  for (let i = minVal; i <= maxVal; i += 2) {
+    if (!arr.includes(i)) {
+      missingOddNumbers.push(i);
+    }
+  }
+  
+  return missingOddNumbers;
+}
+
+// Example usage:
+const arr = [5, 7, 9, 11, 15, 17];
+const missingOdd = findMissingOddNumbers(arr);
+console.log("Missing odd number(s):", missingOdd);
+
