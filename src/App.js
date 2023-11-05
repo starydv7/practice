bb8f1b81bde8bf1c29d9374cc73a8d7cff3b46e9
Pawan Yadav <55851664+starydv7@users.noreply.github.com>
2023-11-05 21:46:48 +0530

    dropdown country and cities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Dropdown from './components/DropDown';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Dropdown/>
     </div>
   );
 }
